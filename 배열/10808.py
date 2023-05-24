48f49f68a0519b78046633d6a93fd3d3168ee425
serak0310 <serak0310@naver.com>
2023-05-24 23:27:03 +0900

    알파벳 개수

---
+++ 배열/10808.py
@@ -0,0 +1,5 @@
+word = input()
+alpahbet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for a in alpahbet:
+    print(word.count(a), end=" ")
