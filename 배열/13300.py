eaf14de48f531cbef0f211071c6732ffebe79cb4
serak0310 <serak0310@naver.com>
2023-05-26 22:15:05 +0900

    방 배정

--- 배열/13300.py
+++ 배열/13300.py
@@ -3,16 +3,15 @@ import math
 
 N, K = list(map(int, input().split()))
 
-students = [[0]*7 for _ in range(3)]  #성별 / 학년별
+students = [[0]*7 for _ in range(2)]
 
 for _ in range(N):
-    S, Y = map(int, input().split())
+    S, Y = list(map(int, input().split()))
     students[S][Y] += 1
 
-room = 0
-
+total = 0
 for i in students:
     for j in i:
-        room += math.ceil(j / K)
+        total += math.ceil(j/K)
 
-print(room)
+print(total)
