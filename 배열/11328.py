9cdff019f2ffc3a25a2cae0a712b4e875652b4fe
serak0310 <serak0310@naver.com>
2023-05-26 22:18:54 +0900

    Strfry

---
+++ 배열/11328.py
@@ -0,0 +1,12 @@
+# Strfry
+
+T = int(input())
+
+for _ in range(T):
+    str1, str2 = input().split()
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+    if str1==str2:
+        print("Possible")
+    else:
+        print("Impossible")
