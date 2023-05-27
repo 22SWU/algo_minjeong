807a85fb54327d17e173c05fea0eeb2ff56f4956
serak0310 <serak0310@naver.com>
2023-05-28 03:20:07 +0900

    1406: 에디터

--- 02. 연결리스트/1406_에디터.py
+++ 02. 연결리스트/1406_에디터.py
@@ -1,46 +1,21 @@
 # 1406: 에디터
-
-# word = list(input())
-# n = int(input())
-
-# cmdPos = len(word)
-
-# for i in range(n):
-#     cmd = input().split()
-#     if cmd[0]=="L":
-#             if cmdPos!=0:
-#                 cmdPos -= 1
-#     elif cmd[0]=="D":
-#         if cmdPos!=len(word):
-#             cmdPos += 1
-#     elif cmd[0]=="B":
-#         if cmdPos!=0:
-#             word.pop(cmdPos-1)
-#         cmdPos -= 1
-#     elif cmd[0]=="P":
-#         newWord = cmd[1]
-#         word.insert(cmdPos, newWord)
-#         cmdPos += 1
-        
-
-# for i in word:
-#     print(i, end="")
-
 from sys import stdin
 
 left = list(input())
 right = []
+n = int(input())
 
-for _ in range(int(input())):
-    command = list(stdin.readline().split())
-    if command[0] == 'L' and left:
+for _ in range(n):
+    cmd = list(stdin.readline().split())
+
+    if cmd[0]=="L" and left:
         right.append(left.pop())
-    elif command[0] == 'D' and right:
+    elif cmd[0]=="D" and right:
         left.append(right.pop())
-    elif command[0] == 'B' and left:
+    elif cmd[0]=="B" and left:
         left.pop()
-    elif command[0] == 'P':
-        left.append(command[1])
-
+    elif cmd[0]=="P":
+        left.append(cmd[1])
+    
 answer = left + right[::-1]
 print(''.join(answer))
