bce3b802043c36845ec01ff935b8f865220ff000
serak0310 <serak0310@naver.com>
2023-05-31 22:19:17 +0900

    3015: 오아시스 재결합

--- 03. 스택/3015_오아시스 재결합.py
+++ 03. 스택/3015_오아시스 재결합.py
@@ -1,38 +1,38 @@
-# # 3015: 오아시스 재결합
+# 3015: 오아시스 재결합
 
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# oasis = [int(input()) for _ in range(int(input()))]
+oasis = [int(input()) for _ in range(int(input()))]
 
-# stack = [] # (키, cnt)로 append
-# result = 0
+stack = [] # (키, cnt)로 append
+result = 0
 
-# for o in oasis:
+for o in oasis:
 
-#   # 스택 끝 값보다 키 크면 pop
-#   while stack and stack[-1][0]<o:
-#     result += stack.pop()[1]
+  # 스택 끝 값보다 키 크면 pop
+  while stack and stack[-1][0]<o:
+    result += stack.pop()[1]
 
-#   # 스택이 비어있으면 해당 키 append하고 continue
-#   if not stack:
-#     stack.append((o, 1))
-#     continue
+  # 스택이 비어있으면 해당 키 append하고 continue
+  if not stack:
+    stack.append((o, 1))
+    continue
 
     
-#   # 스택 끝 값의 키와 같을 때
-#   if stack[-1][0]==o:
-#     cnt = stack.pop()[1]
-#     result += cnt
+  # 스택 끝 값의 키와 같을 때
+  if stack[-1][0]==o:
+    cnt = stack.pop()[1]
+    result += cnt
 
-#     if stack: result += 1
-#     stack.append((o, cnt+1))
+    if stack: result += 1
+    stack.append((o, cnt+1))
 
-#   # 스택 끝 값의 키보다 작을 때
-#   else:
-#     stack.append((o, 1))
-#     result += 1
+  # 스택 끝 값의 키보다 작을 때
+  else:
+    stack.append((o, 1))
+    result += 1
 
-# # 결과값 출력
-# print(result)
+# 결과값 출력
+print(result)
 
