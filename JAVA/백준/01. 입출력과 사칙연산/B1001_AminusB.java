4f75f2f59493b2bca63d841ee4e959388b763946
serak0310 <serak0310@naver.com>
2023-06-08 03:54:41 +0900

    1001: A-B

---
+++ JAVA/백준/01. 입출력과 사칙연산/B1001_AminusB.java
@@ -0,0 +1,18 @@
+// 1001: A-B
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1001_AminusB {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a-b);
+    }
+}
