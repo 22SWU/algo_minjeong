329acf4f4e74af96602392140e15f7a67cdc0d2c
serak0310 <serak0310@naver.com>
2023-06-08 04:09:33 +0900

    10926: ??!

---
+++ JAVA/백준/01. 입출력과 사칙연산/B10926_questionmark.java
@@ -0,0 +1,14 @@
+// 10926: ??!
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10926_questionmark {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str+"??!");
+    }
+}
