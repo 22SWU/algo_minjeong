a7e62f6a1c1f5de64fdc10220cd39f67d696eaa8
serak0310 <serak0310@naver.com>
2023-06-08 14:36:35 +0900

    10951: A+B - 4

--- JAVA/백준/03. 반복문/B10951_AplusB_4.java
+++ JAVA/백준/03. 반복문/B10951_AplusB_4.java
@@ -1,9 +1,23 @@
-// 10951: A+B - 8
+// 10951: A+B - 4
+// https://st-lab.tistory.com/40
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B10951_AplusB_4 {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+
+        while((str=br.readLine())!=null){
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(a+b);
+        }
+    }
 }
