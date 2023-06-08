b0f5af2aeeceb1a37474c14de22e62bba72fc42b
serak0310 <serak0310@naver.com>
2023-06-08 15:44:24 +0900

    5597: 과제 안 내신 분..?

--- JAVA/백준/04. 1차원 배열/B5597_과제안내신분.java
+++ JAVA/백준/04. 1차원 배열/B5597_과제안내신분.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class B5597_과제안내신분 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] student = new int[30];
+        boolean[] student = new boolean[30];
 
         for(int i=0; i<28; i++){
             int cur = Integer.parseInt(sc.nextLine());
-            student[cur-1] = 1;
+            student[cur-1] = true;
         }
 
         for(int i=0; i<student.length; i++){
-            if(student[i]==0){
+            if(!student[i]){
                 System.out.println(i+1);
             } 
         }
