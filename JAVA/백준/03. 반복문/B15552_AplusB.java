9f50978fd86190b418a308c10f8c4c3dddb48361
serak0310 <serak0310@naver.com>
2023-06-08 13:56:29 +0900

    15552: 빠른 A+B

--- JAVA/백준/03. 반복문/B15552_AplusB.java
+++ JAVA/백준/03. 반복문/B15552_AplusB.java
@@ -1,19 +1,25 @@
-// 10950: A+B-3
+// 15552: 빠른 A+B
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class B10950_AplusBminus3 {
+public class B15552_AplusB {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i=0; i<T; i++){
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        for(int i=0; i<num; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            System.out.println(a+b);
-        } 
+            bw.write(a+b+"\n"); 
+        }
+        bw.flush();
+        bw.close(); 
     }
 }
