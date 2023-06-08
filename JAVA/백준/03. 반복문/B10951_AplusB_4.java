dda7a8838b1303846727e42731275033c13a972f
serak0310 <serak0310@naver.com>
2023-06-08 14:17:00 +0900

    10952: A+B - 5

---
+++ JAVA/백준/03. 반복문/B10951_AplusB_4.java
@@ -0,0 +1,9 @@
+// 10951: A+B - 8
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10951_AplusB_4 {
+    
+}
--- JAVA/백준/03. 반복문/B10952_AplusB_5.java
+++ JAVA/백준/03. 반복문/B10952_AplusB_5.java
@@ -1,4 +1,4 @@
-// 10952: A+B - 8
+// 10952: A+B - 5
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
