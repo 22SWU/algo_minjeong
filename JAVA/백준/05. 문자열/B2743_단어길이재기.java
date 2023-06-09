b65809dc0b613f9b76238f8a2b57bfb7486dc2d7
serak0310 <serak0310@naver.com>
2023-06-09 23:36:09 +0900

    2743: 단어 길이 재기

---
+++ JAVA/백준/05. 문자열/B2743_단어길이재기.java
@@ -0,0 +1,12 @@
+// 2743: 단어 길이 재기
+
+import java.util.Scanner;
+
+public class B2743_단어길이재기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(str.length());
+    }
+}
