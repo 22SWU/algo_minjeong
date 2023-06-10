44011b6261d2c74b7bd23447e0fc3600125f84b3
serak0310 <serak0310@naver.com>
2023-06-10 16:38:22 +0900

    1152: 단어의 개수

---
+++ JAVA/백준/05. 문자열/B1152_단어의개수.java
@@ -0,0 +1,14 @@
+// 1152: 단어의 개수
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1152_단어의개수 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        System.out.println(st.countTokens());
+    }
+}
