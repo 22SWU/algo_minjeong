736738e116b3b40b7833d505cad5552fab436291
serak0310 <serak0310@naver.com>
2023-06-13 23:29:01 +0900

    2493: 탑

--- Python/백준/03. 스택/2493_탑.py
+++ Python/백준/03. 스택/2493_탑.py
@@ -1,20 +1,19 @@
 # 2493: 탑
 
-if __name__ == "__main__":
-    N = int(input())  # 탑의 개수
-    top_list = list(map(int, input().split()))  # 탑 리스트
-    stack = []
-    answer = []
+n = int(input())
+tops = list(map(int, input().split()))
 
-    for i in range(N):
-        while stack:
-            if stack[-1][1] > top_list[i]:  # 수신 가능한 상황
-                answer.append(stack[-1][0] + 1)
-                break
-            else:
-                stack.pop()
-        if not stack:  # 스택이 비면 레이저를 수신할 탑이 없다.
-            answer.append(0)
-        stack.append([i, top_list[i]])  # 인덱스, 값
+stack = []
+answer = [0]*n
 
-    print(" ".join(map(str, answer)))
+for i in range(n):
+    while stack:
+        if stack[-1][1] > tops[i]:
+            answer[i] = (stack[-1][0]+1)
+            break
+        else:
+            stack.pop()
+    stack.append([i, tops[i]])
+
+
+print(" ".join(map(str, answer)))
