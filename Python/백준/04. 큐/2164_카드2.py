ded54e944f181ffbf0fcbc054f45d40f833d4065
serak0310 <serak0310@naver.com>
2023-07-10 15:05:54 +0900

    2164: 카드2

--- Python/백준/04. 큐/2164_카드2.py
+++ Python/백준/04. 큐/2164_카드2.py
@@ -2,12 +2,11 @@
 
 from collections import deque
 
-N = int(input())
-deque = deque([i for i in range(1, N+1)])
+n = int(input())
+deq = deque([i for i in range(1, n+1)])
 
-while(len(deque) >1):
-    deque.popleft()
-    move_num = deque.popleft()
-    deque.append(move_num)
-    
-print(deque[0])
+while (len(deq) > 1):
+    deq.popleft()
+    deq.append(deq.popleft())
+
+print(deq[0])
